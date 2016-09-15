15d7d518d758eef4e4c608a44f5771a1d165cad2
Felix Schnizlein <felix@schnizle.in>
2016-09-15 23:47:35 +0200

    Added tox.ini; Prepare pytest infrastructure; move data files

--- setup.py
+++ setup.py
@@ -1,41 +1,39 @@
 #!/usr/bin/env python
 
-from os import path
+import sys
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print("xii requires setuptools in order to be installed.")
+    sys.exit(1)
 
 
 setupdict = dict(
-   name='xii',
-   version='1.0.0',
-   description='Easy virtual machine spawning',
-   url='https://github.com/felixsch/xii',
-   # Author details
-   author='Felix Schnizlein',
-   author_email='felix@schnizle.in',
-   license='GPL-3.0',
-   # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-   classifiers=[
-      'Development Status :: 5 - Production/Stable'
-      #
-      # The license:
-      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-      # Supported Python versions:
-      'Programming Language :: Python :: 2.7'
-   ],
-   keywords='xii',
-   include_package_data = True,
-   packages=find_packages('src'),
-   package_dir={'': 'src'},
-   data_files=[('config', ['default/config.yml'])],
-   extras_require={
-        'test': ['pytest', 'coverage'],
-   },
-   entry_points={
-        'console_scripts': [
-            'xii=xii:main',
-        ],
-    },
-)
+        name='xii',
+        keywords='xii',
+        version='1.0.0',
+        description='Easy virtual machine spawning',
+        url='https://github.com/xii/xii',
+        author='Felix Schnizlein',
+        author_email='xii@schnizle.in',
+        license='GPL-3.0',
+        classifiers=[
+            'Development Status :: 3 - Alpha'
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Operating System :: POSIX',
+            'Topic :: Utilities',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            ],
+        install_requires=['paramiko', "PyYAML", 'setuptools', 'pycrypto'],
+        include_package_data=True,
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        data_files=[('config', ['defaults/config.yml'])],
+        entry_points={'console_scripts': ['xii=xii:main']}
+        )
 
 setup(**setupdict)
