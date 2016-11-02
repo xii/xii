dab37080671fa25fc798ade7ed8e4159a93a2f61
Felix Schnizlein <felix@schnizle.in>
2016-11-02 19:56:32 +0100

    Minor cleanup

--- setup.py
+++ setup.py
@@ -10,29 +10,28 @@ except ImportError:
 
 
 setupdict = dict(
-        name='xii',
-        keywords='xii',
-        version='1.0.0',
-        description='Easy virtual machine spawning',
-        url='https://github.com/xii/xii',
-        author='Felix Schnizlein',
-        author_email='xii@schnizle.in',
-        license='GPL-3.0',
+        name="xii",
+        keywords=["xii", "virtualization"],
+        version="1.0.0",
+        description="A easy to use virtual machine manager",
+        url="https://github.com/xii/xii",
+        author="Felix Schnizlein",
+        author_email="xii@schnizle.in",
+        license="GPL-3.0",
         classifiers=[
-            'Development Status :: 3 - Alpha'
-            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Environment :: Console',
-            'Intended Audience :: Developers',
-            'Operating System :: POSIX',
-            'Topic :: Utilities',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
+            "Development Status :: 3 - Alpha"
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Operating System :: POSIX",
+            "Topic :: Utilities",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
             ],
-        install_requires=['paramiko', "PyYAML", 'setuptools', 'pycrypto'],
+        install_requires=["paramiko", "PyYAML", "pycrypto"],
         include_package_data=True,
-        packages=find_packages('src'),
-        package_dir={'': 'src'},
-        entry_points={'console_scripts': ['xii=xii:main']}
+        packages=find_packages("src"),
+        package_dir={"": "src"},
+        entry_points={"console_scripts": ["xii=xii:main"]}
         )
-
 setup(**setupdict)
