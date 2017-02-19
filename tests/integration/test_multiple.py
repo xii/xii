b19d09daf5bb7204358b06f433baf5e484e24069
Felix Schnizlein <felix@schnizle.in>
2017-02-19 17:51:53 +0100

    Added ssh connection to integration tests

--- tests/integration/test_multiple.py
+++ tests/integration/test_multiple.py
@@ -1,13 +1,23 @@
 import pytest
 
+from integration import runner, file, ssh
 
-from integration import runner, file
+hosts = ["multiple-1", "multiple-2"]
 
-
-@runner.cleanup_node(["multiple-1", "multiple-2", "multiple-3"], "qemu:///system")
+@runner.cleanup_node(hosts, "qemu:///system")
 def test_multi_nodes():
+    """ 
+    run 3 instances at the same time. Make sure ssh works correctly
+    and hostname is set to instance name
+    """
+
     env_vars = runner.load_variables_from_env()
     multiple   = file.get_test_path("multiple")
  
     runner.run_xii(deffile=multiple, variables=env_vars, cmd="start")
+
+    for host in hosts:
+        con = ssh.connect_to(host, "root", "linux", multiple)
+        assert(con.run("hostname") == host)
+
     runner.run_xii(deffile=multiple, variables=env_vars, cmd="destroy")
