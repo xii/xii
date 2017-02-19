a8699ed958bc796a59e2d38c6dc34e859906f963
Felix Schnizlein <felix@schnizle.in>
2017-02-19 17:51:53 +0100

    Added ssh connection to integration tests

--- tests/integration/integration/ssh.py
+++ tests/integration/integration/ssh.py
@@ -1,28 +1,41 @@
 import subprocess
+import socket
 import paramiko
 import pytest
 
 from time import sleep
 
+def connect_to(domain, user, pw, deffile):
+    return SSH(deffile, domain, user, password=pw, keyfile=None)
 
-def new_ssh(domain, user=None):
-    pass
+def find_host_ip(domain, deffile):
+    proc = subprocess.Popen(["xii", "--deffile", deffile, "ip", "--quiet", domain],
+            stdout=subprocess.PIPE)
+    (out, _) = proc.communicate()
 
+    if out.startswith("[xii]"):
+        pytest.fail("Could not fetch valid ip address...")
+    return out
 
 class SSH():
-    def __init__(domain, user):
+    def __init__(self, deffile, domain, user, password=None, keyfile=None):
         self._domain = domain
         self._user   = user
+        self._def    = deffile
+        self._pwd    = password
+        self._key    = keyfile
+        self._ssh    = None
+        self._sftp   = None
 
-    def run(command):
-        conn = self.ssh()
+    def run(self, command):
+        conn = self.connect()
         chan = conn.get_transport().open_session()
         chan.set_combine_stderr(True)
         chan.exec_command(command)
         stdout = chan.makefile('r', -1)
-        return (chan, stdout)
+        return stdout.read().strip()
 
-    def file_contains(path, pattern):
+    def file_contains(self, path, pattern):
         hdl = self.sftp().open(path)
         result = re.match(pattern, hdl.read())
         hdl.close()
@@ -40,13 +53,13 @@ class SSH():
             raise
         return True
 
-    def connect(key=None, password=None):
+    def connect(self):
         if self._ssh:
             return self._ssh
 
-        host = self._find_host()
+        host = find_host_ip(self._domain, self._def)
 
-        for _ in range(20):
+        for _ in range(40):
             try:
                 client = paramiko.SSHClient()
                 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -58,18 +71,14 @@ class SSH():
                     paramiko.AuthenticationException,
                     paramiko.SSHException,
                     socket.error) as e:
-                sleep(2)
-        pytest.xfail("ssh connect failed: Could not connect to host `{}`".format(host))
+                print("[ssh] Connecting to {}...".format(self._domain))
+                sleep(5)
+        pytest.fail("ssh connect failed: Could not connect to host `{}`".format(host))
 
     def sftp(self):
         if self._sftp:
             return self._sftp
 
-        self._sftp = self._ssh.open_sftp()
+        self._sftp = self.connect().open_sftp()
         return self._sftp
-
-    def _find_host(self):
-        process = subprocess.call(["xii", "ip", "--quiet", self._domain])
-        ip = process.stdout.read()
-        
 
--- tests/integration/test_single.py
+++ tests/integration/test_single.py
@@ -1,13 +1,19 @@
 import pytest
 
 
-from integration import runner, file
+from integration import runner, file, ssh
 
 
 @runner.cleanup_node("single", "qemu:///system")
 def test_single_domain():
+    """
+    Test simple one instance run and if password user creation works
+    """
+
     env_vars = runner.load_variables_from_env()
     single   = file.get_test_path("single")
  
     runner.run_xii(deffile=single, variables=env_vars, cmd="start")
-    #runner.run_xii(deffile=single, variables=env_vars, cmd="destroy")
+    con = ssh.connect_to("single", "root", "linux", single)
+    assert(con.stat("/etc/shadow"))
+    runner.run_xii(deffile=single, variables=env_vars, cmd="destroy")
