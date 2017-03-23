4ba3b6da80102b58a64a03ff5c263cde2623ffaa
Felix Schnizlein <felix@schnizle.in>
2017-03-23 13:59:50 +0100

    Document qemu forward-hook and fix iptables rules generation

--- src/xii/builtin/components/forward/templates/forward-hook.py
+++ src/xii/builtin/components/forward/templates/forward-hook.py
@@ -2,125 +2,132 @@
 
 import xml.etree.ElementTree as etree
 import libvirt
-import socket
 import subprocess
 import sys
 import os
 
-""" xii's forwarding script
-TODO
+""" xii's port forwarding script
+simple port forwading script which is used as hook for the libvirt qemu
+implementation.
+
+Args:
+    name    name of the domain which is handled
+    action  action which should be executred (start/stopped/restarted)
+
+Returns:
+    returns 0 on succcess and 1 on error
 """
 
 # def fetch_host_ip():
+#     """ a easy way to get used external ip
+#
+#     Returns:
+#         nothing
+#     """
 #     # snippet from stackoverflow 166506
 #     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #     sock.connect(("8.8.8.8", 0))
 #     return sock.getsockname()[0]
 
-    # /sbin/iptables -I FORWARD -o virbr0 -d  $GUEST_IP -j ACCEPT
-	# /sbin/iptables -t nat -I PREROUTING -p tcp --dport $HOST_PORT -j DNAT --to $GUEST_IP:$GUEST_PORT
-
-
-    #     iptables(["-t", "nat", "-A", dnat_chain, "-p", protocol,
-    #               "-d", public_ip, "--dport", str(public_port),
-    #               "-j", "DNAT", "--to", dest])
-
-    #     iptables(["-t", "nat", "-A", snat_chain, "-p", protocol,
-    #               "-s", private_ip, "-d", private_ip, "--dport", str(public_port), "-j", "MASQUERADE"])
-
-    #     iptables(["-t", "filter", "-A", fwd_chain, "-p", protocol,
-    #               "-d", private_ip, "--dport", str(private_port), "-j", "ACCEPT"] + interface)
-
-
-
-
-    #     iptables(["-t", "nat", "-A", "OUTPUT", "-p", proto,
-    #               "-d", pub, "--dport", pub_p, "-j", "DNAT", "--to", dest])
-
-    #     iptables(["-t", "nat", "-A", "POSTROUTING", "-p", proto,
-    #               "-s", priv, "-d", priv, "--dport",
-    #               pub_p, "-j", "MASQUERADE"])
-
-    #     iptables(["-t", "filter", "-A", "FORWARD", "-p", proto,
-    #               "-d", priv, "--dport", priv_p, "-j", "ACCEPT"])
-# <xii:forwards>
-#   <xii:port-forward src="80" dest="8088"></xii:port-forward>
-# </xii:forwards>
-
-# <xii:node xmlns:xii="http://xii-project.org/xmlns/node/1.0">
-#   <xii:definition>/files/dev/xii/examples/port-forward/port-forward.xii</xii:definition>
-#   <xii:image>6d454c441f13163d6b395b6af5bbd3e0</xii:image>
-#   <xii:user>felixsch</xii:user>
-#   <xii:created>1490092697.18</xii:created>
-# <xii:forwards>
-#   <xii:port-forward src="80" dest="8088"></xii:port-forward>
-# </xii:forwards>
-# </xii:node>
-
 IPTABLES_BIN = subprocess.check_output(["which", "iptables"]).strip()
 
 
 def iptables(args):
-    print("iptables {}".format(" ".join(args)))
+    """ run iptables with additional arguments
+
+    Args:
+        args    iptables arguments as list
+
+    Returns:
+        nothing
+    """
     subprocess.call([IPTABLES_BIN] + args)
 
 
-def default(key, struct, default=None):
-    if key not in struct:
-        return default
-    return struct[key]
-
-
 def sysctl_set(path, value):
-    print("{} -> {}".format(path, value))
+    """set values in /proc path
+
+    Args:
+        path    path which should be altered
+        value   value which should be written
+
+    Returns:
+        Nothing
+    """
     if (os.path.isfile(path)):
         with open(path, 'w') as brnf:
             brnf.write('{}\n'.format(value))
 
 
-def set_rule(name, conn, inst, meta, action):
-    # add forwardings to chains
+def set_rule(ip, meta, action):
+    """set iptables rules for forwarding a port to a virtual maschine
+
+    Args:
+        ip      ipv4 address of the domain
+        meta    portforwarding metadata of the domain
+        action  iptables action ("-I" or "-A" or "-D")
+
+    Returns:
+        nothing
+    """
     for forward in meta:
-        guest_ip = inst[1]
+        guest_ip = ip
         guest_port = forward["guest-port"]
         host_port = forward["host-port"]
-        proto = default("proto", forward, "tcp")
+        proto = forward["proto"] if "proto" in forward else "tcp"
+
         dest = "{}:{}".format(guest_ip, guest_port)
-        interface = ["-o", forward["interface"]] if "interface" in forward else []
-        host_ip = ["-d", forward["host-ip"]] if "host-ip" in forward else []
 
-        # iptables(["-t", "nat", action, "OUTPUT", "-p", "tcp", "-o", "lo",
-        #           "--dport", host_port, "-j", "REDIRECT", "--to-ports", guest_port])
+        iptables(["-t", "nat", action, "OUTPUT", "-p", "tcp", "--dport",
+                  host_port, "-j", "DNAT", "--to-destination", dest])
 
-        iptables(["-t", "nat", action, "OUTPUT", "-p", proto] + host_ip +
-                  ["--dport", host_port, "-j", "DNAT", "--to", dest])
-
-        iptables(["-t", "nat", action, "POSTROUTING", "-p", proto,
-                  "-s", guest_ip, "-d", guest_ip, "--dport",
-                  host_port, "-j", "MASQUERADE"])
-
-        iptables(["-t", "filter", action, "FORWARD", "-p", proto,
-                  "-d", guest_ip, "--dport", guest_port, "-j", "ACCEPT"])
-
-        # iptables([action, "FORWARD", "-d", guest_ip, "-m", "state", "--state",
-        #           "NEW,ESTABLISHED,RELATED", "-j", "ACCEPT"] + interface)
-
-        # iptables(["-t", "nat", action, "PREROUTING", "-p", proto,] + host_ip +
-        #          ["--dport", host_port, "-j", "DNAT", "--to", dest])
+        iptables(["-t", "nat", action, "POSTROUTING", "-p", proto, "-d",
+                  guest_ip, "--dport", guest_port, "-j", "MASQUERADE"])
 
 
-def enable_forwarding(name, conn, inst, meta):
+def enable_forwarding(ip, meta):
+    """enable port forwarding for one domain and set
+       required sysctl flags
+
+    Args:
+        ip      ipv4 address of the domain
+        meta    portforwarding metadata of the domain
+
+    Returns:
+        nothing
+    """
     sysctl_set("/proc/sys/net/bridge/bridge-nf-call-iptables", 0)
     sysctl_set("/proc/sys/net/ipv4/ip_forward", 1)
-    set_rule(name, conn, inst, meta, "-A")
+    sysctl_set("/proc/sys/net/ipv4/conf/all/route_localnet", 1)
+    set_rule(ip, meta, "-A")
 
 
-def disable_forwarding(name, conn, inst, meta):
+def disable_forwarding(ip, meta):
+    """disable port forwarding for one domain
+
+    Args:
+        ip      ipv4 address of the domain
+        meta    portforwarding metadata of the domain
+
+    Returns:
+        nothing
+    """
     set_rule(name, conn, inst, meta, "-D")
 
 
 def parse_meta(inst):
-    ns = { "xii": "http://xii-project.org/xmlns/node/1.0" }
+    """ parse xii meta data from a domain
+
+    It fetches the metadata from the xml description of
+    a domain.
+
+    Args:
+        inst    domain instance object
+
+    Returns:
+        the portforwardings as list of dicts
+    """
+    ns = {"xii": "http://xii-project.org/xmlns/node/1.0"}
     root = etree.fromstring(inst[0].XMLDesc())
     meta = root.find("metadata")
 
@@ -129,7 +136,16 @@ def parse_meta(inst):
 
     return map(lambda x: x.attrib, meta.findall("xii:node/forwards/port", ns))
 
+
 def connect_libvirt(url="qemu:///system"):
+    """connect to libvirt system
+
+    Args:
+        url     the connection url to the _qemu_ system
+
+    Returns:
+        the libvirt connection object
+    """
     try:
         return libvirt.open(url)
     except libvirt.libvirtError as e:
@@ -137,6 +153,15 @@ def connect_libvirt(url="qemu:///system"):
 
 
 def fetch_domain(conn, name):
+    """fetch domain data from libvirt connection
+
+    Args:
+        conn    libvirt connection object
+        name    name of the domain/node/instance
+
+    Returns:
+        a tuple with (libvirt domain object, ip4v address of the domain)
+    """
     try:
         domain = conn.lookupByName(name)
         nets = domain.interfaceAddresses(
@@ -153,11 +178,14 @@ def fetch_domain(conn, name):
     except libvirt.libvirtError as e:
         raise RuntimeError("could not find domain {}.".format(e))
 
+# Run this actions on different qemu states
 ACTION_TABLE = {
     "start": [enable_forwarding],
     "stopped": [disable_forwarding],
     "reconnect": [disable_forwarding, enable_forwarding]
 }
+
+
 if __name__ == "__main__":
     name, action = sys.argv[1:3]
     try:
