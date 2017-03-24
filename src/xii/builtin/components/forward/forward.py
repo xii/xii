70496f4b9fc754cc9afde6fcca6caa0965cf0141
Felix Schnizlein <felix@schnizle.in>
2017-03-24 13:30:35 +0100

    Started implementing hook installation

--- src/xii/builtin/components/forward/forward.py
+++ src/xii/builtin/components/forward/forward.py
@@ -1,10 +1,11 @@
 import libvirt
 
-from xii import error
+from xii import error, util
 from xii.component import Component
 from xii.need import NeedLibvirt, NeedIO
-from xii.util import flatten
 
+DEFAULT_PATH = "/etc/libvirt/hooks/qemu"
+FORWARD_HOOK = "forward-hook.py"
 
 class ForwardComponent(Component, NeedLibvirt, NeedIO):
     """Define forwards like portforwarding
@@ -17,21 +18,34 @@ class ForwardComponent(Component, NeedLibvirt, NeedIO):
     default_attributes = []
     requires = []
 
+    def hook_path(self):
+        return self.config("hooks/port_forward_path", DEFAULT_PATH)
+
     def hook_exists(self):
-        return True
-        pass
+        return self.io().exists(self.hook_path())
 
     def install_hook(self):
+        # TODO: Implement me
         pass
 
     def forwards_for(self, instance):
-        forwards = self.each_attribute("forwards_for", args={"instance": instance})
-        return "\n".join(flatten(forwards))
+        forwards = self.each_attribute(
+            "forwards_for",
+            args={"instance": instance})
+        return "\n".join(util.flatten(forwards))
 
     def spawn(self):
-        if not self.hook_exists():
-            self.install_hook()
-        self.each_attribute("spawn")
+        pass
+        # if (not self.hook_exists() or
+        #     not self._hook_check_sha(self.hook_path(), FORWARD_HOOK)):
+        #     self.install_hook()
+        #     self.restart_libvirtd()
 
-    def destroy(self):
-        self.each_attribute("destroy")
+    def _hook_check_sha(self, path, template_name):
+        p_hash = hashlib.sha256()
+        t_hash = hashlib.sha256()
+
+        p = p_hash.update(util.file_read(path))
+        t = t_hash.update(self.template(template_name))
+
+        return p == t
