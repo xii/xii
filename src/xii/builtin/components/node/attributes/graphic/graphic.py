b23ecb5e5473501c7161d710511002239b11b0cb
Felix Schnizlein <felix@schnizle.in>
2017-05-01 19:05:30 +0200

    Document nodes/attributes/graphic

--- src/xii/builtin/components/node/attributes/graphic/graphic.py
+++ src/xii/builtin/components/node/attributes/graphic/graphic.py
@@ -4,9 +4,27 @@ from xii.components.node import NodeAttribute
 
 
 class GraphicAttribute(NodeAttribute):
+    """Attach spice output to the created node.
+    By simply adding the graphich line xii is instructed to add a vnc spice
+    connection to the started node.
+
+    Currently only qxl/spice output is supported.
+    """
     atype = "graphic"
     keys = Bool(True)
     defaults = True
+
+    example = """
+    # vim: set ts=2 sw=2 tw=0 ft=yaml:
+    ---
+    with-graphic:
+    type: node
+    pool: default
+
+    image: {{ image }}
+
+    graphic: yes
+  """
 
     def spawn(self):
         if not self.settings():
