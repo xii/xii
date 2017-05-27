60183174f8c914ceff867eeab8ac64c262e37e49
Felix Schnizlein <felix@none.io>
2017-05-27 02:13:54 +0200

    Implement basic list command

--- src/xii/builtin/commands/list/list.py
+++ src/xii/builtin/commands/list/list.py
@@ -1,3 +1,5 @@
+import datetime
+
 from xii import definition, command, error
 from xii.need import NeedLibvirt, NeedSSH
 
@@ -25,5 +27,33 @@ class ListCommand(command.Command):
                             help="Show only secified components [nodes,pools,networks]")
         return parser
 
+    def _get_uptime(self, time):
+        now  = datetime.datetime.now()
+        delta = now - datetime.datetime.fromtimestamp(time)
+
+        if delta.days > 1:
+            return "{} days".format(delta.days)
+
+        if delta.seconds / 3600 > 1:
+            return "{} hours".format(delta.seconds / 3600)
+
+        if delta.seconds / 60 > 1:
+            return "{} minutes".format(delta.seconds / 60)
+        return "{} seconds".format(delta.seconds)
+
     def run(self):
-        pass
+        rows = []
+        for c in self.children():
+            meta = c.fetch_metadata()
+
+            create = "---"
+            if meta is not None:
+                created_at = float(meta["created"])
+                create = self._get_uptime(created_at)
+
+            rows.append((c.entity(),
+                c.get_virt_url(),
+                create,
+                c.status()
+                ))
+        self.show_table(["name", "host", "uptime", "status"], rows)
