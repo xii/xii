54bebb040548b54cbb71d93ae840c9e114e5f093
Felix Schnizlein <felix@schnizle.in>
2017-09-29 16:18:56 +0200

    ansible: add make ip fetching more reliable

--- src/xii/builtin/components/ansible/ansible_attribute.py
+++ src/xii/builtin/components/ansible/ansible_attribute.py
@@ -1,4 +1,8 @@
 from xii import attribute
 
 class AnsibleAttribute(attribute.Attribute):
-    pass
+    def get_virt_url(self):
+        return self.component().get_virt_url()
+
+    def get_tmp_volume_path(self):
+        return self.other_attribute("image").get_tmp_volume_path()
